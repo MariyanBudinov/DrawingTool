44d8a5e59ba3f9460f6e1ab137125ebe28dcefd4
Momchil Lambovski <momchillambovski@gmail.com>
2017-04-21 14:28:21 +0300

    asd

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var monk = require('monk');
+var db = monk("mongodb://<dbuser>:<dbpassword>@ds157980.mlab.com:57980/drawingtooldb-mm");
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
-var monk = require('monk');
-var db = monk("mongodb://<dbuser>:<dbpassword>@ds157980.mlab.com:57980/drawingtooldb-mm");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -23,8 +25,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/login', login);
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
