3da81f205bde9635907ab7d57f2dd1055bf32e1f
Momchil Lambovski <momchillambovski@gmail.com>
2017-05-01 02:28:20 +0300

    asd

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var mongodb = require('mongodb');
 var monk = require('monk');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
-var fbLogin = require('./routes/passport')(app,passport);
+var fbLogin = require('./routes/passport')(app, passport);
 var db = monk("mongodb://mariyan:1234@ds157980.mlab.com:57980/drawingtooldb-mm");
 
 
@@ -46,79 +46,79 @@ app.use(session({
     saveUninitialized: true
 }));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// passport.serializeUser(function (user, done) {
-//     done(null, user._id);
-// });
+passport.serializeUser(function (user, done) {
+    done(null, user._id);
+});
 
-// passport.deserializeUser(function (_id, done) {
-//     var facebookUsers = db.get('facebookUsers');
-//     facebookUsers.findById(_id, function (err, user){
-//         done(err, user);
-//     });
-// });
+passport.deserializeUser(function (_id, done) {
+    var facebookUsers = db.get('facebookUsers');
+    facebookUsers.findById(_id, function (err, user) {
+        done(err, user);
+    });
+});
 
-// passport.serializeUser(function (user, done) {
-//     done(null, user);
-// });
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
 
-// passport.deserializeUser(function (user, done) {
-//     done(null, user);
-// });
-
-
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
 
 
 
 
 
-//Facebook Login
-// var FACEBOOK_APP_ID = 1487666597973757;
-// var FACEBOOK_SECRET = '4aeefd80f5a57fbf189882ef94e35eb7';
 
 
-// passport.use(new FacebookStrategy({
-//     clientID: FACEBOOK_APP_ID,
-//     clientSecret: FACEBOOK_SECRET,
-//     callbackURL: "http://drawing-tool-mm.herokuapp.com/auth/facebook/callback"
-//     //  "http://localhost:3000/auth/facebook/callback" || 
-// },
-//     function (accessToken, refreshToken, profile, done) {
-//         var facebookUsers = db.get('facebookUsers');
-//         facebookUsers.findOne({
-//             'facebook.id': profile.id
-//         }, function (err, user) {
-//             if (err) {
-//                 return done(err);
-//             }
-//             if (!user) {
-//                 facebookUsers.insert(
-//                     ({
-//                         name: profile.displayName,
+Facebook Login
+var FACEBOOK_APP_ID = 1487666597973757;
+var FACEBOOK_SECRET = '4aeefd80f5a57fbf189882ef94e35eb7';
 
-//                         username: profile.username,
-//                         provider: 'facebook',
 
-//                         facebook: profile._json
-//                     }));
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_SECRET,
+    callbackURL: "http://drawing-tool-mm.herokuapp.com/auth/facebook/callback"
+    //  "http://localhost:3000/auth/facebook/callback" || 
+},
+    function (accessToken, refreshToken, profile, done) {
+        var facebookUsers = db.get('facebookUsers');
+        facebookUsers.findOne({
+            'facebook.id': profile.id
+        }, function (err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                facebookUsers.insert(
+                    ({
+                        name: profile.displayName,
 
-//                 return done(null, user);
-//             } else {
-//                 return done(null, user);
-//             }
-//         });
-//     }
-// ));
+                        username: profile.username,
+                        provider: 'facebook',
 
-// app.get('/auth/facebook', passport.authenticate('facebook'));
+                        facebook: profile._json
+                    }));
 
-// app.get('/auth/facebook/callback',
-//     passport.authenticate('facebook', {
-//         successRedirect: 'http://drawing-tool-mm.herokuapp.com',
-//         failureRedirect: 'http://drawing-tool-mm.herokuapp.com/login'
-//     }));
+                return done(null, user);
+            } else {
+                return done(null, user);
+            }
+        });
+    }
+));
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+        successRedirect: 'http://drawing-tool-mm.herokuapp.com',
+        failureRedirect: 'http://drawing-tool-mm.herokuapp.com/login'
+    }));
 
 
 
--- routes/passport.js
+++ routes/passport.js
@@ -1,79 +1,79 @@
-var express = require('express');
-var router = express.Router();
-var session = require('express-session');
-var mongodb = require('mongodb');
-var monk = require('monk');
-var passport = require('passport');
-var FacebookStrategy = require('passport-facebook').Strategy;
+// var express = require('express');
+// var router = express.Router();
+// var session = require('express-session');
+// var mongodb = require('mongodb');
+// var monk = require('monk');
+// var passport = require('passport');
+// var FacebookStrategy = require('passport-facebook').Strategy;
 
-//Facebook Login
-module.exports = function (app, passport) {
+// //Facebook Login
+// module.exports = function (app, passport) {
 
-    router.use(passport.initialize());
-    router.use(passport.session());
-    router.use(session({
-        secret: 'momchil',
-        resave: true,
-        saveUninitialized: true
-    }));
+//     app.use(passport.initialize());
+//     app.use(passport.session());
+//     app.use(session({
+//         secret: 'momchil',
+//         resave: true,
+//         saveUninitialized: true
+//     }));
 
-    passport.serializeUser(function (user, done) {
-        done(null, user._id);
-    });
+//     passport.serializeUser(function (user, done) {
+//         done(null, user._id);
+//     });
 
-    passport.deserializeUser(function (_id, done) {
-        var facebookUsers = db.get('facebookUsers');
-        facebookUsers.findById(_id, function (err, user) {
-            done(err, user);
-        });
-    });
+//     passport.deserializeUser(function (_id, done) {
+//         var facebookUsers = db.get('facebookUsers');
+//         facebookUsers.findById(_id, function (err, user) {
+//             done(err, user);
+//         });
+//     });
 
-    var FACEBOOK_APP_ID = 1487666597973757;
-    var FACEBOOK_SECRET = '4aeefd80f5a57fbf189882ef94e35eb7';
+//     var FACEBOOK_APP_ID = 1487666597973757;
+//     var FACEBOOK_SECRET = '4aeefd80f5a57fbf189882ef94e35eb7';
 
 
-    passport.use(new FacebookStrategy({
-        clientID: FACEBOOK_APP_ID,
-        clientSecret: FACEBOOK_SECRET,
-        callbackURL: "http://drawing-tool-mm.herokuapp.com/auth/facebook/callback"
-        //  "http://localhost:3000/auth/facebook/callback" || 
-    },
-        function (accessToken, refreshToken, profile, done) {
-            var facebookUsers = db.get('facebookUsers');
-            facebookUsers.findOne({
-                'facebook.id': profile.id
-            }, function (err, user) {
-                if (err) {
-                    return done(err);
-                }
-                if (!user) {
-                    facebookUsers.insert(
-                        ({
-                            name: profile.displayName,
+//     passport.use(new FacebookStrategy({
+//         clientID: FACEBOOK_APP_ID,
+//         clientSecret: FACEBOOK_SECRET,
+//         callbackURL: "http://drawing-tool-mm.herokuapp.com/auth/facebook/callback"
+//         //  "http://localhost:3000/auth/facebook/callback" || 
+//     },
+//         function (accessToken, refreshToken, profile, done) {
+//             var facebookUsers = db.get('facebookUsers');
+//             facebookUsers.findOne({
+//                 'facebook.id': profile.id
+//             }, function (err, user) {
+//                 if (err) {
+//                     return done(err);
+//                 }
+//                 if (!user) {
+//                     facebookUsers.insert(
+//                         ({
+//                             name: profile.displayName,
 
-                            username: profile.username,
-                            provider: 'facebook',
+//                             username: profile.username,
+//                             provider: 'facebook',
 
-                            facebook: profile._json
-                        }));
+//                             facebook: profile._json
+//                         }));
 
-                    return done(null, user);
-                } else {
-                    return done(null, user);
-                }
-            });
-        }
-    ));
+//                     return done(null, user);
+//                 } else {
+//                     return done(null, user);
+//                 }
+//             });
+//         }
+//     ));
 
-    router.get('/auth/facebook', passport.authenticate('facebook'));
+//     router.get('/auth/facebook', passport.authenticate('facebook'));
 
-    router.get('/auth/facebook/callback',
-        passport.authenticate('facebook', {
-            successRedirect: 'http://drawing-tool-mm.herokuapp.com',
-            failureRedirect: 'http://drawing-tool-mm.herokuapp.com/login'
-        }));
-    return passport;
-}
+//     router.get('/auth/facebook/callback',
+//         passport.authenticate('facebook', {
+//             successRedirect: 'http://drawing-tool-mm.herokuapp.com',
+//             failureRedirect: 'http://drawing-tool-mm.herokuapp.com/login'
+//         }));
+//     return passport;
+// }
 
 
 
