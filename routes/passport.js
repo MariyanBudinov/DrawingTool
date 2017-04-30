e20c592c69ed1d4cf83bd844abcb838284d14e8d
Momchil Lambovski <momchillambovski@gmail.com>
2017-05-01 02:06:34 +0300

    asd

--- routes/passport.js
+++ routes/passport.js
@@ -9,64 +9,72 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 //Facebook Login
 module.exports.facebookLogin = function (req, res, next) {
 
-    router.use(passport.initialize());
-    router.use(passport.session());
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     passport.serializeUser(function (user, done) {
-        done(null, user);
+        done(null, user._id);
     });
 
-    passport.deserializeUser(function (user, done) {
-        done(null, user);
+    passport.deserializeUser(function (_id, done) {
+        var facebookUsers = db.get('facebookUsers');
+        facebookUsers.findById(_id, function (err, user) {
+            done(err, user);
+        });
     });
+
     var FACEBOOK_APP_ID = 1487666597973757;
     var FACEBOOK_SECRET = '4aeefd80f5a57fbf189882ef94e35eb7';
+
 
     passport.use(new FacebookStrategy({
         clientID: FACEBOOK_APP_ID,
         clientSecret: FACEBOOK_SECRET,
-        callbackURL: "http://localhost:3000/auth/facebook/callback" || "http://drawing-tool-mm.herokuapp.com/auth/facebook/callback"
+        callbackURL: "http://drawing-tool-mm.herokuapp.com/auth/facebook/callback"
+        //  "http://localhost:3000/auth/facebook/callback" || 
     },
         function (accessToken, refreshToken, profile, done) {
             var facebookUsers = db.get('facebookUsers');
-
             facebookUsers.findOne({
                 'facebook.id': profile.id
             }, function (err, user) {
                 if (err) {
                     return done(err);
                 }
-                //No user was found... so create a new user with values from Facebook (all the profile. stuff)
                 if (!user) {
                     facebookUsers.insert(
                         ({
                             name: profile.displayName,
-                            //email: profile.emails[0].value,
+
                             username: profile.username,
                             provider: 'facebook',
-                            //now in the future searching on User.findOne({'facebook.id': profile.id } will match because of this next line
+
                             facebook: profile._json
                         }));
-                    // user.save(function (err) {
-                    //     if (err) console.log(err);
-                    //     return done(err, user);
-                    // });
-                    return done(err, user);
+
+                    return done(null, user);
                 } else {
-                    //found user. Return
-                    return done(err, user);
+                    return done(null, user);
                 }
             });
         }
     ));
 
-router.get('/auth/facebook', passport.authenticate('facebook'));
+    app.get('/auth/facebook', passport.authenticate('facebook'));
 
-router.get('/auth/facebook/callback',
-    passport.authenticate('facebook', {
-        successRedirect: '/',
-        failureRedirect: '/login'
-    }));
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect: 'http://drawing-tool-mm.herokuapp.com',
+            failureRedirect: 'http://drawing-tool-mm.herokuapp.com/login'
+        }));
+
+    router.get('/auth/facebook', passport.authenticate('facebook'));
+
+    router.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect: '/',
+            failureRedirect: '/login'
+        }));
 }
 
 
