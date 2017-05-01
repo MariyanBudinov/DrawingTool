61ae686c813af48e78500274b81262beab273029
MariyanBudinov <mariqn_b@live.com>
2017-05-01 19:09:59 +0300

    Upload images button

--- public/javascripts/functionality.js
+++ public/javascripts/functionality.js
@@ -49,6 +49,21 @@ $(window).on("load", function() {
         }
     });
 
+    $('#openFile').change(function(e) {
+        var file = e.target.files[0];
+        var reader = new FileReader();
+        reader.onload = function(f) {
+            var data = f.target.result;
+            fabric.Image.fromURL(data, function(img) {
+                var oImg = img.set({ left: 50, top: 50, angle: 0 }).scale(0.9);
+                canvas.add(oImg).renderAll();
+                var objectActive = canvas.setActiveObject(oImg);
+                var dataURL = canvas.toDataURL({ format: 'png', quality: 0.8 });
+            });
+        };
+        reader.readAsDataURL(file);
+    });
+
     $("#savePNG").click(function() {
         canvas.isDrawingMode = false;
         drawingModeEl.innerHTML = '<img src="/images/paletteIcon.png" width=20 > Enter Drawing';
